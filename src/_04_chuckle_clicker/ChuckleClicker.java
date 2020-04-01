76ec54deb50af338045d2ba30ccacf575abcf4bb
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-04-01 15:10:42 -0700

    Chuckle Clicker, Typing Tutor(IP)

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,19 +1,39 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
-	public static void main(String[] args) {
-		makeButtons();
+public class ChuckleClicker implements ActionListener {
+	JButton joke = new JButton("Joke");
+	JButton punchline = new JButton("Punchline");
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	ChuckleClicker() {
 	}
-	static void makeButtons() {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+	void run() {
 		frame.add(panel);
+		panel.add(joke);
+		panel.add(punchline);
+		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JButton joke = new JButton("Joke");
-		JButton punchline = new JButton("Punchline");
+		joke.addActionListener(this);
+		punchline.addActionListener(this);
+		frame.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if(buttonPressed == joke) {
+			JOptionPane.showMessageDialog(null, "Today, my son asked 'Can I have a book Mark?' and I burst into tears. 11 years old and he still doesn't know my name is Brian.");
+		}
+		if(buttonPressed == punchline) {
+			JOptionPane.showMessageDialog(null, "Why does Waldo wear stripes? Because he doesn’t want to be spotted.");
+		}
 	}
 }
--- src/_04_chuckle_clicker/ChuckleClickerRunner.java
+++ src/_04_chuckle_clicker/ChuckleClickerRunner.java
@@ -1,5 +1,8 @@
 package _04_chuckle_clicker;
 
 public class ChuckleClickerRunner {
-
+	public static void main(String[] args) {
+		ChuckleClicker run = new ChuckleClicker();
+		run.run();
+	}
 }
