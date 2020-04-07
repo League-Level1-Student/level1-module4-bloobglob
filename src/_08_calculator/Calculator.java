326e3abbe24ac960e9f1a64e53ff19b9a343ca8f
bloobglob <50851263+bloobglob@users.noreply.github.com>
2020-04-06 17:48:32 -0700

    Calculator(IP)

--- src/_08_calculator/Calculator.java
+++ src/_08_calculator/Calculator.java
@@ -1,11 +1,17 @@
 package _08_calculator;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator {
+public class Calculator implements MouseListener {
 	void run() {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
@@ -15,6 +21,8 @@ public class Calculator {
 		JButton subtract = new JButton("subtract");
 		JButton multiply = new JButton("multiply");
 		JButton divide = new JButton("divide");
+		add.addMouseListener(this);
+		JLabel label;
 		frame.add(panel);
 		panel.add(num1);
 		panel.add(num2);
@@ -25,5 +33,35 @@ public class Calculator {
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
